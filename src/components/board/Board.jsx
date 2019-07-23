d532c8980e8953a22fa2b5625ca579da58ead06e
Himalee <himalee.tailor@gmail.com>
2019-07-23 11:57:25 +0100

    Add: ability to mark board with X

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import {Cell} from '../cell/Cell'
+import * as cellValue from '../../cellValue';
 
-export function Board(props) {
-  return props.grid.map((cell, index) => {
-    return <Cell
-      cellLabel={props.grid[index]}
-    />
-  });
+export class Board extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      grid: Array(9).fill(cellValue.EMPTY)
+    };
+  }
+
+  handleClick(gridIndex) {
+    const grid=this.state.grid.slice();
+    grid[gridIndex]=cellValue.X;
+    this.setState({grid: grid});
+  }
+
+  renderGrid() {
+    const updatedGrid=this.state.grid.map((cell, index) =>
+        <Cell
+          cellLabel={this.state.grid[index]}
+          onClick={() => this.handleClick(index)}
+          key={index}
+        />
+    )
+    return (
+      <div>{updatedGrid}</div>
+    );
+  }
+
+  render() {
+    return (
+      this.renderGrid()
+    );
+  }
 }
--- test/Board.test.js
+++ test/Board.test.js
@@ -6,6 +6,12 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
 
 it('displays 9 cells for a 3x3 board', () => {
-  const wrapper = Enzyme.shallow(<Board grid={[0, 1, 2, 3, 4, 5, 6, 7, 8]}/>);
+  const wrapper = Enzyme.shallow(<Board grid={Array(9).fill("")}/>);
   expect(wrapper.find(Cell)).toHaveLength(9);
+});
+
+it('marks board with X on click', () => {
+  const wrapper = Enzyme.shallow(<Board grid={Array(9).fill("")}/>);
+  wrapper.find(Cell).first().simulate('click');
+  expect(wrapper.state('grid')).toEqual(['X', "", "", "", "", "", "", "", ""]);
 });
