17063085964b675d720e6b806c2a2d5dbb8d6ec7
Himalee <himalee.tailor@gmail.com>
2019-07-25 17:04:29 +0100

    Refactor: change name of Board prop to handleClick

--- src/components/cell/Cell.jsx
+++ src/components/cell/Cell.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './cell.css';
 
 export function Cell(props) {
-  return <button id={props.id}
-    onClick={props.onClick}>
-    {props.cellLabel}
+  return (
+    <button id={props.id} onClick={props.onClick}>
+      {props.cellLabel}
     </button>
+  );
 }
--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,4 +1,4 @@
-import React, {Component}  from 'react';
+import React, {Component} from 'react';
 import {Board} from '../board/Board';
 import * as boardHelper from '../board/boardHelper';
 import * as gameHelper from '../game/gameHelper';
@@ -6,19 +6,22 @@ import * as lineAnalysis from '../../lineAnalysis';
 import * as cellValue from '../../cellValue';
 
 export class Game extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      grid: Array(this.props.boardSize).fill(cellValue.EMPTY)
+    this.state = {
+      grid: Array(this.props.boardSize).fill(cellValue.EMPTY),
     };
-    this.handleClick=this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    const index=e.target.id;
-    const grid=this.state.grid.slice();
-    if (boardHelper.isMoveAvailable(grid, index) && !lineAnalysis.isGameOver(grid)) {
-      grid[index]=gameHelper.determineMark(this.state.grid)
+    const index = e.target.id;
+    const grid = this.state.grid.slice();
+    if (
+      boardHelper.isMoveAvailable(grid, index) &&
+      !lineAnalysis.isGameOver(grid)
+    ) {
+      grid[index] = gameHelper.determineMark(this.state.grid);
     }
     this.setState({grid: grid});
   }
@@ -26,8 +29,7 @@ export class Game extends Component {
   render() {
     return (
       <div>
-      <Board updatedGrid={this.state.grid}
-        onClick={this.handleClick}/>
+        <Board updatedGrid={this.state.grid} handleClick={this.handleClick} />
         <h2>{gameHelper.status(this.state.grid)}</h2>
       </div>
     );
