db195e13c56e6d944fcea3dc141d471182082fef
Himalee <himalee.tailor@gmail.com>
2019-07-30 14:50:41 +0100

    Add: hide gameMode buttons once a gameMode has been selected

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -8,40 +8,33 @@ export class TicTacToe extends Component {
     super(props);
     this.state = {
       gameMode: null,
+      gameModeChosen: false,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
     const chosenGameMode = e.target.id;
+    this.setState({gameModeChosen: true});
     this.setState({gameMode: chosenGameMode});
-  }
-
-  createGame(gameMode) {
-    return <Game board={Array(9).fill(EMPTY)} gameMode={gameMode} />;
-  }
-
-  setUpGame() {
-    switch (this.state.gameMode) {
-      case gameMode.HUMANVSHUMAN:
-        return this.createGame(gameMode.HUMANVSHUMAN);
-      case gameMode.HUMANVSRANDOM:
-        return this.createGame(gameMode.HUMANVSRANDOM);
-      default:
-        return 'Choose a game mode';
-    }
   }
 
   render() {
     return (
       <div>
-        <button id={gameMode.HUMANVSHUMAN} onClick={this.handleClick}>
-          Human
-        </button>
-        <button id={gameMode.HUMANVSRANDOM} onClick={this.handleClick}>
-          Random
-        </button>
-        {this.setUpGame()}
+        {!this.state.gameModeChosen ? (
+          <div>
+            <button id={gameMode.HUMANVSHUMAN} onClick={this.handleClick}>
+              Human
+            </button>
+            <button id={gameMode.HUMANVSRANDOM} onClick={this.handleClick}>
+              Random
+            </button>
+          </div>
+        ) : null}
+        {this.state.gameModeChosen ? (
+          <Game board={Array(9).fill(EMPTY)} gameMode={this.state.gameMode} />
+        ) : null}
       </div>
     );
   }
