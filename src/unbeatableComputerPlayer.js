1094a9ac635e23b386ceb5c2cc31338e47ce5e0d
Himalee <himalee.tailor@gmail.com>
2019-07-31 18:08:58 +0100

    Add: unbeatable computer player

---
+++ src/unbeatableComputerPlayer.js
@@ -0,0 +1,41 @@
+import { availableMoves } from '../src/components/board/boardHelper';
+import { determineMark } from '../src/components/game/gameHelper';
+import * as lineAnalysis from '../src/lineAnalysis';
+
+export function getMove(grid, depth, mark) {
+  let scores = {};
+  if (lineAnalysis.isThereADraw(grid)) {
+    return 0;
+  } else if (lineAnalysis.checkForWinner(grid).winnerFound) {
+    return -(1000 / depth);
+  }
+  availableMoves(grid).forEach(move => {
+    let newGrid = markGrid(grid.slice(), move, mark);
+    let opponentMark = determineMark(grid);
+    scores[move] = -getMove(newGrid, depth + 1, opponentMark);
+  });
+  return getBestMoveOrBestScore(scores, depth);
+}
+
+function getBestMoveOrBestScore(scores, depth) {
+  if (depth === 1) {
+    return getBestMove(scores);
+  }
+  return getBestScore(scores);
+}
+
+function markGrid(grid, move, mark) {
+  grid[move] = mark;
+  return grid;
+}
+
+function getBestScore(scores) {
+  return Math.max.apply(null, Object.values(scores));
+}
+
+function getBestMove(scores) {
+  return parseInt(
+    Object.keys(scores).reduce((a, b) => (scores[a] > scores[b] ? a : b)),
+    10,
+  );
+}
---
+++ test/unbeatableComputerPlayer.test.js
@@ -0,0 +1,32 @@
+import * as unbeatableComputerPlayer from '../src/unbeatableComputerPlayer';
+import { EMPTY } from '../src/cellValue';
+
+it('player X returns last remaining move', () => {
+  const grid = ['X', 'O', 'X', 'O', 'O', EMPTY, 'O', 'X', 'X'];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'X')).toEqual(5);
+});
+
+it('player O returns winning move with 2 moves left', () => {
+  const grid = ['X', 'O', 'X', 'O', 'O', EMPTY, EMPTY, 'X', 'X'];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'O')).toEqual(5);
+});
+
+it('player X returns winning move with 3 moves left', () => {
+  const grid = ['O', 'X', EMPTY, 'X', 'X', EMPTY, EMPTY, 'O', 'O'];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'X')).toEqual(5);
+});
+
+it('player X returns winning move with 5 moves remaining', () => {
+  const grid = ['O', 'X', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, 'X', 'O'];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'X')).toEqual(4);
+});
+
+it('player X blocks player O from winning', () => {
+  const grid = ['O', 'O', EMPTY, 'X', EMPTY, EMPTY, EMPTY, 'X', EMPTY];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'X')).toEqual(2);
+});
+
+it('player X chooses to win over blocking opponent from winning', () => {
+  const grid = ['X', 'X', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, 'O', 'O'];
+  expect(unbeatableComputerPlayer.getMove(grid, 1, 'X')).toEqual(2);
+});
